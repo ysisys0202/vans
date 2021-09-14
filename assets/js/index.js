f7a306d5208b8fe25812ab610c44f46a5a3dfee9
ysisys0202 <ysisys0202@gmail.com>
2021-09-15 06:56:58 +0900

    ✨ main slide 추가

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const mainSlider = tns({
+  container: ".main-slide-list",
+  items: 1,
+  mode: "gallery",
+  autoplay: true,
+  autoplayTimeout: 7000,
+  controls: false,
+  nav: false,
+  autoplayButtonOutput: false,
+});
